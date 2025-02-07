d045bcab84e5dfbc5660b2f0858cdf3c117f9778
RemiArata <50224993+RemiArata@users.noreply.github.com>
2025-02-07 14:59:51 -0700

    kinda works but now how I want. Always returns a 404 but it does create the url

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
   * It is an architexture style focused around stateless client side communications, uniform interfaces, and predefined operations
 * RESTful APIs are preferred for building distributed systems, mobile appications, and web services
   * they provide scalibility, client-friendliness, and flexability
+
+## URL Shortener
+* A service that shortens a stardard URL to one with more managable size and length is called a URL shortener
+* Shortened URLs reference the longer one when clicked on
+* URL shortener use code as a stand in for the full text URL
--- shortener.go
+++ shortener.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,11 +13,48 @@ import (
 this starts a server and run here: http://127.0.0.1:8080/hello
 */
 
+var URLMap = make(map[string]string)
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	r := gin.Default()
 	r.GET("/hello", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "Hello World"})
 	})
 
+	r.GET("/shorten/:url", shortenURL)
+	r.GET("/:shortURL", goToShortUrl)
+
 	r.Run(":8080")
+}
+
+func shortenURL(c *gin.Context) {
+	longURL := c.Param("url")
+	shortURL := generateShortURL()
+	URLMap[shortURL] = longURL
+	fmt.Println(URLMap)
+	c.JSON(http.StatusOK, gin.H{"shortURL": shortURL})
+}
+
+func generateShortURL() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	const size = len(charset)
+	short := make([]byte, 6)
+	for i := range short {
+		short[i] = charset[rand.Intn(size)]
+	}
+	return string(short)
+}
+
+func goToShortUrl(c *gin.Context) {
+	shortURL := c.Param("shortURL")
+	fmt.Println("the short url ", shortURL)
+	longURL, ok := URLMap[shortURL]
+	fmt.Println(ok)
+	if !ok {
+		c.JSON(http.StatusNotFound, gin.H{"message": "short URL doesn't have corrosponding long URL"})
+		return
+	}
+	fmt.Println(longURL)
+	c.Redirect(http.StatusMovedPermanently, longURL)
 }
